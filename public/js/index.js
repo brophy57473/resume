1773285c2777719b31b99b3b78b99e8d52bc6d48
brophy57473 <61244635+brophy57473@users.noreply.github.com>
2021-03-25 15:35:01 -0400

    add scroll function

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,34 @@
 $(function(){
 
-    $('#green-cap').on('click', function(){
-        console.log('clicked');
+    $('.capsule:not(.selected)').on('click', function(){
+        $(this).addClass('selected');
+        $('.capsule:not(.selected)').animate({opacity: '0%'}, 500);
+        $('.title-box').animate({opacity: '0%'}, 500);
+        setTimeout(function(){
+            $('.capsule.selected').removeClass('selected');
+            $('.capsule').animate({opacity: '100%'}, 500);
+            $('.title-box').animate({opacity: '100%'}, 500);
+        },3000)
     })
 
+    $('a[href]').on('click',function(e){
+        e.preventDefault();
+        let id = $(this).attr('href').substring(1);
+        $('html, body').animate({
+                scrollTop: $('#' + id).offset().top 
+        }, 1000);
+    })
+
+    $('.to-top').on('click',function(){
+        $(window).scrollTop(0);
+    })
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+            $('.to-top').fadeIn();
+        } else {
+            $('.to-top').fadeOut();
+        }
+    })
     
 })
